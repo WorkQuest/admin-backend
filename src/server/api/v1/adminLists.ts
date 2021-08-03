84519e8a7a9137396e6c6fbd9d26b426c4a27cc6
Irina <prilepovaid@mail.ru>
2021-08-03 12:19:55 +0700

    Add scelete of functions to show quests-,users-,black lists

--- src/server/api/v1/adminLists.ts
+++ src/server/api/v1/adminLists.ts
@@ -3,8 +3,31 @@ import {Admin, Role} from "database-models/lib/models/Admin"
 import { paginate } from "../../utils";
 import { Errors } from "../../utils/errors";
 
-
+//TODO здесь бд админа, нужно бд юзера, когда появится
 export async function showUsersList(r) {
+  if(r.auth.credentials.adminRole !== Role.main){
+    return error(Errors.InvalidAdminType, 'Invalid admin type', {})
+  }
+  const users = await Admin.findAndCountAll({
+    ...paginate(r),
+    order: [['createdAt', 'DESC']]
+  })
+  return output({data: users.rows, count: users.count})
+}
+
+export async function showQuestsList(r) {
+  if(r.auth.credentials.adminRole !== Role.main){
+    return error(Errors.InvalidAdminType, 'Invalid admin type', {})
+  }
+  const users = await Admin.findAndCountAll({
+    ...paginate(r),
+    order: [['createdAt', 'DESC']]
+  })
+  return output({data: users.rows, count: users.count})
+}
+
+
+export async function isInBlackList(r) {
   if(r.auth.credentials.adminRole !== Role.main){
     return error(Errors.InvalidAdminType, 'Invalid admin type', {})
   }
--- src/server/routes/v1/adminLists.ts
+++ src/server/routes/v1/adminLists.ts
@@ -1,6 +1,6 @@
 //здесь будет список всех юзеров, чёрный список, списко квестов
 import * as Joi from "joi";
-import { showUsersList } from "../../api/v1/adminLists"
+import { showUsersList, showQuestsList } from "../../api/v1/adminLists"
 import { adminRoleSchema, } from "database-models/lib/schemes/admin";
 import { emailSchema, firstNameSchema, lastNameSchema, paginationFields, idSchema} from "database-models/lib/schemes/common";
 import { outputPaginationSchema, outputOkSchema } from "../../schemes";
@@ -14,15 +14,36 @@ export const userInfoSchema = Joi.object({
   adminRole: adminRoleSchema,
 })
 
+export const questInfoSchema = Joi.object({
+  
+})
+
 //TODO: ДОПОЛНИТЬ СХЕМУ ВЫВОДА ИНФЫ О ЮЗЕРАХ, СЕЙЧАС ЗДЕСЬ СХЕМА АДМИНА
 export default[{
   method: "GET",
   path: "/v1/admin/usersList",
   handler: showUsersList,
   options: {
-    id: "v1.admin.getUserList",
+    id: "v1.admin.getUsersList",
     tags: ["api", "admin"],
     description: "Show a list of users",
+    validate: {
+      query: Joi.object({
+        ...paginationFields
+      }).label('PaginationFields')
+    },
+    response: {
+      schema: outputPaginationSchema('transactionsList', userInfoSchema).label('GetListOfTrasactionsResponseSchema')
+    }
+  }
+}, {
+  method: "GET",
+  path: "/v1/admin/questsList",
+  handler: showQuestsList,
+  options: {
+    id: "v1.admin.getQuestsList",
+    tags: ["api", "admin"],
+    description: "Show a list of quests",
     validate: {
       query: Joi.object({
         ...paginationFields
