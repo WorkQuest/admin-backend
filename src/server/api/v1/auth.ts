bc1c48cea8ca39694a377824c5e2f49ef8a6449e
Irina Prilepova <prilepovaid@mail.ru>
2021-08-04 18:25:11 +0700

    Improve

--- src/server/api/v1/auth.ts
+++ src/server/api/v1/auth.ts
@@ -3,6 +3,7 @@ import { output, error} from "../../utils";
 import { Admin, Session, } from "@workquest/database-models/lib/models"
 import { generateJwt, checkExisting } from "../../utils/auth";
 import { Op } from "sequelize";
+import {AdminSession} from "@workquest/database-models/lib/models/AdminSession";
 
 export async function login(r) {
   const account = await Admin.scope("withPassword").findOne({
@@ -25,8 +26,8 @@ export async function login(r) {
     throw error(Errors.InvalidTOTP, "Invalid TOTP", {});
   }
 
-  const session = await Session.create({
-    userId: account.id
+  const session = await AdminSession.create({
+    adminId: account.id
   });
 
   return output({ ...generateJwt({ id: session.id })});
