666b4172f78dbb82edd043e16b270381b33b612b
Irina Prilepova <prilepovaid@mail.ru>
2021-11-10 16:38:25 +0700

    Refactor quest.ts

--- src/server/api/v1/quests.ts
+++ src/server/api/v1/quests.ts
@@ -1,25 +1,26 @@
 import {
   Quest,
   QuestStatus,
-  QuestsResponse, QuestDispute,
+  QuestsResponse, QuestMedia,
 } from "@workquest/database-models/lib/models";
 import {error, output} from "../../utils";
 import {Errors} from "../../utils/errors";
 import { getMedias } from "../../utils/medias";
 
-export async function getQuestsList(r){
+export async function getQuestsList(r) {
   const {rows, count} = await Quest.findAndCountAll({
     limit: r.query.limit,
     offset: r.query.offset,
   });
+
   return output({ count, quests: rows });
 }
 
 export async function questInfo(r) {
-
   const quest = await Quest.findByPk(r.params.questId);
+
   if(!quest) {
-    error(Errors.NotFound, 'Quest not found',{});
+    return error(Errors.NotFound, 'Quest not found',{});
   }
 
   return output(quest);
@@ -32,13 +33,9 @@ export async function getUserQuestsInfo(r) {
     },
     limit: r.query.limit,
     offset: r.query.offset,
-  })
+  });
 
-  if(!quests) {
-    return error(Errors.NotFound, "Disputes are not found", {});
-  }
-
-  return output({ count: quests.count, disputes: quests.rows });
+  return output({ count: quests.count, quests: quests.rows });
 }
 
 export async function editQuest(r) {
@@ -46,7 +43,7 @@ export async function editQuest(r) {
   const transaction = await r.server.app.db.transaction();
 
   if(!quest) {
-    error(Errors.NotFound, 'Quest not found',{});
+    return error(Errors.NotFound, 'Quest not found',{});
   }
   quest.mustHaveStatus(QuestStatus.Created);
 
@@ -60,9 +57,7 @@ export async function editQuest(r) {
   await quest.update(r.payload, { transaction });
   await transaction.commit();
 
-  return output(
-    await Quest.findByPk(quest.id)
-  )
+  return output(quest);
 }
 
 export async function deleteQuest(r) {
@@ -76,7 +71,9 @@ export async function deleteQuest(r) {
     return error(Errors.InvalidStatus, "Quest cannot be deleted at current stage", {});
   }
 
-  await QuestsResponse.destroy({ where: { questId: quest.id }, transaction })
+  //TODO maybe made quest and response paranoid?
+  await QuestsResponse.destroy({ where: { questId: quest.id }, transaction });
+  await QuestMedia.destroy({ where: { questId: quest.id } });
   await quest.destroy({ force: true, transaction });
 
   await transaction.commit();
