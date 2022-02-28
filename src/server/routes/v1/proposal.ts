fcbc3153c637019c8e1b56ecc2bbaffbd8f59c81
Microsxema <RobertGtr@protonmail.com>
2022-02-28 18:36:28 +0700

    update poposals

--- src/server/routes/v1/proposal.ts
+++ src/server/routes/v1/proposal.ts
@@ -9,43 +9,41 @@ import {
   outputPaginationSchema,
 } from '@workquest/database-models/lib/schemes';
 
-export default [
-  {
-    method: 'GET',
-    path: '/v1/proposals',
-    handler: handlers.getProposals,
-    options: {
-      auth: 'jwt-access',
-      plugins: getRbacSettings(AdminRole.main),
-      id: 'v1.getProposals',
-      tags: ['api', 'proposal'],
-      description: 'Get proposals',
-      validate: {
-        query: proposalQuerySchema.label('GetProposalsQuery'),
-      },
-      response: {
-        schema: outputPaginationSchema('proposals', proposalSchema).label('GetProposalsResponse'),
-      },
+export default [{
+  method: 'GET',
+  path: '/v1/proposals',
+  handler: handlers.getProposals,
+  options: {
+    auth: 'jwt-access',
+    plugins: getRbacSettings(AdminRole.main),
+    id: 'v1.getProposals',
+    tags: ['api', 'proposal'],
+    description: 'Get proposals',
+    validate: {
+      query: proposalQuerySchema.label('GetProposalsQuery'),
+    },
+    response: {
+      schema: outputPaginationSchema('proposals', proposalSchema).label('GetProposalsResponse'),
     },
   },
-  {
-    method: 'GET',
-    path: '/v1/user/{userId}/proposals',
-    handler: handlers.getProposals,
-    options: {
-      auth: 'jwt-access',
-      plugins: getRbacSettings(AdminRole.main),
-      id: 'v1.getUserProposals',
-      tags: ['api', 'proposal'],
-      description: 'Get proposals by user',
-      validate: {
-        params: Joi.object({
-          userId: idSchema.required(),
-        }).label('GetUserProposalsParams'),
-        query: proposalQuerySchema.label('GetUserProposalsByUserQuery'),
-      },
-      response: {
-        schema: outputPaginationSchema('proposals', proposalSchema).label('GetUserProposalsByUserResponse'),
-      },
+}, {
+  method: 'GET',
+  path: '/v1/user/{userId}/proposals',
+  handler: handlers.getProposals,
+  options: {
+    auth: 'jwt-access',
+    plugins: getRbacSettings(AdminRole.main),
+    id: 'v1.getUserProposals',
+    tags: ['api', 'proposal'],
+    description: 'Get proposals by user',
+    validate: {
+      params: Joi.object({
+        userId: idSchema.required(),
+      }).label('GetUserProposalsParams'),
+      query: proposalQuerySchema.label('GetUserProposalsQuery'),
     },
+    response: {
+      schema: outputPaginationSchema('proposals', proposalSchema).label('GetUserProposalsResponse'),
+    },
+  },
 }];
