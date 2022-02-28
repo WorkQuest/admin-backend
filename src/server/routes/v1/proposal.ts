039e33b4819c466b41ce8ba0cb716d26140db2d5
Irina Prilepova <prilepovaid@mail.ru>
2022-02-28 18:24:06 +0700

    refactor

--- src/server/routes/v1/proposal.ts
+++ src/server/routes/v1/proposal.ts
@@ -12,40 +12,40 @@ import {AdminRole} from "@workquest/database-models/lib/models";
 export default [
   {
     method: 'GET',
-    path: '/v1/user/statistic/dao/proposal',
+    path: '/v1/proposal',
     handler: handlers.getDaoStatistic,
     options: {
       auth: 'jwt-access',
       plugins: getRbacSettings(AdminRole.main),
-      id: 'v1.getDaoStatistic',
+      id: 'v1.getProposals',
       tags: ['api', 'statistic'],
-      description: 'Get dao proposals statistic',
+      description: 'Get proposals statistic',
       validate: {
-        query: proposalQuerySchema.label('GetDaoProposalsStatisticQuery'),
+        query: proposalQuerySchema.label('GetProposalsStatisticQuery'),
       },
       response: {
-        schema: outputPaginationSchema('proposals', proposalSchema).label('GetDaoProposalsStatisticResponse'),
+        schema: outputPaginationSchema('proposals', proposalSchema).label('GetProposalsStatisticResponse'),
       },
     },
   },
   {
     method: 'GET',
-    path: '/v1/user/statistic/{userId}/dao/proposal',
+    path: '/v1/user/{userId}/proposal',
     handler: handlers.getDaoStatistic,
     options: {
       auth: 'jwt-access',
       plugins: getRbacSettings(AdminRole.main),
-      id: 'v1.getUserDaoProposalsStatistic',
+      id: 'v1.getUserProposalsStatistic',
       tags: ['api', 'statistic'],
-      description: 'Get dao proposals statistic for user',
+      description: 'Get proposals statistic for user',
       validate: {
         params: Joi.object({
           userId: idSchema.required(),
-        }).label('GetUserDaoProposalsStatisticParams'),
-        query: proposalQuerySchema.label('GetUserDaoProposalsStatisticForUserQuery'),
+        }).label('GetUserProposalsStatisticParams'),
+        query: proposalQuerySchema.label('GetUserProposalsStatisticForUserQuery'),
       },
       response: {
-        schema: outputPaginationSchema('proposals', proposalSchema).label('GetUserDaoProposalsStatisticForUserResponse'),
+        schema: outputPaginationSchema('proposals', proposalSchema).label('GetUserProposalsStatisticForUserResponse'),
       },
     },
   },
