f959f582b6d9d114d8d47a24d630558db3fada03
Microsxema <RobertGtr@protonmail.com>
2022-06-06 17:35:43 +0700

    [update]

--- src/server/index.ts
+++ src/server/index.ts
@@ -15,7 +15,7 @@ import { tokenValidate, } from './utils/auth';
 import SwaggerOptions from './config/swagger';
 import { pinoConfig, } from './config/pino';
 import { initDatabase } from "@workquest/database-models/lib/models";
-import {QueueClient} from "@workquest/workers-queue-client/lib/client";
+import {QueueClient} from "@workquest/workers-queue-client";
 import {ControllerBroker} from "./controllers/controller.broker";
 const HapiSwagger = require('hapi-swagger');
 const Package = require('../../package.json');
