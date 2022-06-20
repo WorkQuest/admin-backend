db6be4c15fe670ef437d98a6dede58b5fbbb81f4
Irina Prilepova <prilepovaid@mail.ru>
2022-06-20 11:33:37 +0700

    refactor(admin): add logic to change admin role

--- src/server/handlers/admin/ChangeAdminRoleHandler.ts
+++ src/server/handlers/admin/ChangeAdminRoleHandler.ts
@@ -1,38 +1,61 @@
 import {BaseDomainHandler, HandlerDecoratorBase, IHandler} from "../types";
-import { TakeQuestDisputeCommand, TakeQuestDisputeResult } from "../quest/dispute/types";
-import { AdminRole, DisputeStatus } from "@workquest/database-models/lib/models";
 import { AdminAccessPermission } from "./AdminAccessPermission";
+import { ChangeAdminRoleCommand, ChangeAdminRoleResult } from "./types";
+import { AdminChangeRoleData } from "@workquest/database-models/lib/models/admin/AdminChangeRoleData";
+import { AdminValidator } from "./AdminValidator";
 
-export interface ChangeAdminRoleCommand {
-  readonly adminId: string;
-  readonly role: AdminRole;
+export interface ChangeAdminRolePayload extends ChangeAdminRoleCommand {
+
 }
 
-export interface ChangeAdminRolePayload()
+export class ChangeAdminRoleHandler extends BaseDomainHandler<ChangeAdminRoleCommand, ChangeAdminRoleResult> {
+  public async Handle(command: ChangeAdminRoleCommand): ChangeAdminRoleResult {
+    await AdminChangeRoleData.create({
+      changedByAdminId: command.changeRoleByAdmin.id,
+      adminId: command.changeRoleAdmin.id,
+      movedFromRole: command.changeRoleAdmin.role,
+    }, { transaction: this.options.tx });
 
-export class ChangeAdminRoleHandler extends BaseDomainHandler<TakeQuestDisputeCommand, TakeQuestDisputeResult> {
-
-  private static async changeAdminRole(ChangeAdminRoleCommand)
-
-
-  public async Handle(command: ChangeAdminRoleCommand): TakeQuestDisputeResult {
-
+    await command.changeRoleAdmin.update({
+      role: command.moveToRole,
+    }, { transaction: this.options.tx });
   }
 }
 
-export class ChangeAdminRolePreAccessHandler extends HandlerDecoratorBase<TakeQuestDisputeCommand, TakeQuestDisputeResult> {
+export class ChangeAdminRolePreValidateHandler extends HandlerDecoratorBase<ChangeAdminRoleCommand, ChangeAdminRoleResult> {
+
+  private readonly validator: AdminValidator;
+
+  constructor(
+    protected readonly decorated: IHandler<ChangeAdminRoleCommand, ChangeAdminRoleResult>,
+  ) {
+    super(decorated);
+
+    this.validator = new AdminValidator();
+  }
+
+  public async Handle(command: ChangeAdminRoleCommand): ChangeAdminRoleResult {
+    this.validator.NotMailRole(command.moveToRole);
+    this.validator.CantChangeSelfRole(command.changeRoleByAdmin.id,command.changeRoleAdmin.id);
+
+    return this.decorated.Handle(command);
+  }
+}
+
+export class ChangeAdminRolePreAccessPermissionHandler extends HandlerDecoratorBase<ChangeAdminRoleCommand, ChangeAdminRoleResult> {
 
   private accessPermission: AdminAccessPermission;
 
   constructor(
-    protected readonly decorated: IHandler<TakeQuestDisputeCommand, TakeQuestDisputeResult>
+    protected readonly decorated: IHandler<ChangeAdminRoleCommand, ChangeAdminRoleResult>
   ) {
     super(decorated);
 
     this.accessPermission = new AdminAccessPermission();
   }
 
-  public async Handle(command: TakeQuestDisputeCommand): TakeQuestDisputeResult {
+  public async Handle(command: ChangeAdminRoleCommand): ChangeAdminRoleResult {
+    await this.accessPermission.HasNoActiveDisputes(command.changeRoleAdmin.id);
     return this.decorated.Handle(command);
   }
 }
